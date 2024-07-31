a43df00d205a01380e12d84d794916f5dbaeb1ff
shikham123 <shikha.mishra@programming.com>
2024-07-31 13:11:09 +0530

    Add files via upload

---
+++ InvoiceCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.test.invoice_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceCheckerApplication.class, args);
+	}
+
+}
